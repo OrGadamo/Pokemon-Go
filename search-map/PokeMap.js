8a5a9ee2a08af5a2d1530fc552a9d14e39399eee
OrGadamo <brber223@gmail.com>
2022-07-31 01:46:02 +0300

    satarday- progress

--- search-map/PokeMap.js
+++ search-map/PokeMap.js
@@ -158,12 +158,10 @@ function getPokemonInfo(pokemon, index) {
         </a>
       </div>
       <div class="card-body">
-        <h5 class="card-title">${pokemon.name}</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#!" onclick="changeToScene(${index})" class="btn btn-primary">Capture</a>
+      <div class="d-flex justify-content-between">
+      <h5 class="card-title">${pokemon.name}</h5>
+      <a href="#!" onclick="changeToScene(${index})" class="btn btn-primary">Capture</a>
+      </div>
       </div>
     </div>
   </div>
